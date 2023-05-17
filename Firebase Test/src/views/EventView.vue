6334d2afcc11b2316c792cf83a442a7479dbb1a4
andkoehlert <118164444+andkoehlert@users.noreply.github.com>
2023-05-17 14:18:20 +0200

    Working on issues

--- Firebase Test/src/views/EventView.vue
+++ Firebase Test/src/views/EventView.vue
@@ -9,7 +9,9 @@
   <div class="">
     
     <div class="relative">
-   
+  
+<!-- Video-->
+
         <video autoplay muted loop id="myVideo">
   <source src="../assets/4k.mp4" type="video/mp4">
 </video>
@@ -17,74 +19,94 @@
             
             <div class="logo ml-60   mix-blend-mode: overlay;" style="  mix-blend-mode: overlay;
 ">
+
+<!-- IMG-->
+
+
     <img src="../assets/Kanten.png" class="logo ml-20 h-56 w-56 object-scale-down	" style="mix-blend-mode: overlay;
 " alt="">
   </div>
-            <hr>
-            <ul class="flex items-center gap-[10vw]  ">
+
+<hr class="line">
+  <!-- Nav-->
+  
+            <ul class="home_nav md:flex	 items-center gap-[10vw] 	  ">
     <li>
-      <a class="hover:text-red-500 text-white text-lg" href="#">Home</a>
+      <a class="hover:text-white text-white text-2xl font-bold transition
+     duration-150 border-b-8 border-transparent 
+     hover:border-purple-500" href="#">Home</a>
     </li>
     <li>
-      <a class="hover:text-red-500 text-white text-lg" >Events</a>
+      <a class="hover:text-white text-white text-2xl font-bold transition
+     duration-150 border-b-8 border-transparent 
+     hover:border-purple-500" >Events</a>
     </li>
     <li>
-      <a class="hover:text-red-500 text-white text-lg" href="#">Gallery</a>
+      <a class="hover:text-white text-white text-2xl font-bold transition
+     duration-150 border-b-8 border-transparent 
+     hover:border-purple-500" href="#">Gallery</a>
     </li>
     <li>
-      <a class="hover:text-red-500 text-white text-lg" href="#">About us</a>
+      <a class="hover:text-white text-white text-2xl font-bold transition
+     duration-150 border-b-8 border-transparent 
+     hover:border-purple-500" href="#">About us</a>
     </li>
     <li>
-      <a class="hover:text-red-500 text-white text-lg" href="#">Contact</a>
+      <a class="hover:text-white text-white text-2xl font-bold transition
+     duration-150 border-b-8 border-transparent 
+     hover:border-purple-500" href="#">Contact</a>
     </li>
   </ul>
-        <hr>
+        <hr class="line">
         <div class="container m-auto flex grid grid-cols-1 content-around">
            
             <div class=" flex items-center justify-center">
-                <button
-  type="button"
-  data-te-ripple-init
-  data-te-ripple-color="light"
-  class="mb-2 inline-block rounded-full p-3 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
-  style="background-color: #1877f2">
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    class="h-4 w-4"
-    fill="currentColor"
-    viewBox="0 0 24 24">
-    <path
-      d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
-  </svg>
+               
+ <!-- Buttons socials-->
+             
+ <button
+ type="button"
+ data-te-ripple-init
+ data-te-ripple-color="light"
+ class="mb-2 inline-block rounded-full p-3 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
+ style="background-color: #1877f2">
+ <svg
+   xmlns="http://www.w3.org/2000/svg"
+   class="h-4 w-4"
+   fill="currentColor"
+   viewBox="0 0 24 24">
+   <path
+     d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
+ </svg>
 </button>
 <button
   type="button"
   data-te-ripple-init
   data-te-ripple-color="light"
   class="mb-2 inline-block rounded-full p-3 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
-  style="background-color: #1877f2">
+  style="background-color: #c13584">
   <svg
-    xmlns="http://www.w3.org/2000/svg"
+  xmlns="http://www.w3.org/2000/svg"
     class="h-4 w-4"
     fill="currentColor"
     viewBox="0 0 24 24">
     <path
-      d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
+    d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
   </svg>
 </button>
-<button
+  <button
   type="button"
   data-te-ripple-init
   data-te-ripple-color="light"
   class="mb-2 inline-block rounded-full p-3 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
-  style="background-color: #1877f2">
+  style="background-color: #1769ff">
   <svg
-    xmlns="http://www.w3.org/2000/svg"
+  xmlns="http://www.w3.org/2000/svg"
     class="h-4 w-4"
     fill="currentColor"
     viewBox="0 0 24 24">
     <path
-      d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
+    d="M19.54 0c1.356 0 2.46 1.104 2.46 2.472v21.528l-2.58-2.28-1.452-1.344-1.536-1.428.636 2.22h-13.608c-1.356 0-2.46-1.104-2.46-2.472v-16.224c0-1.368 1.104-2.472 2.46-2.472h16.08zm-4.632 15.672c2.652-.084 3.672-1.824 3.672-1.824 0-3.864-1.728-6.996-1.728-6.996-1.728-1.296-3.372-1.26-3.372-1.26l-.168.192c2.04.624 2.988 1.524 2.988 1.524-1.248-.684-2.472-1.02-3.612-1.152-.864-.096-1.692-.072-2.424.024l-.204.024c-.42.036-1.44.192-2.724.756-.444.204-.708.348-.708.348s.996-.948 3.156-1.572l-.12-.144s-1.644-.036-3.372 1.26c0 0-1.728 3.132-1.728 6.996 0 0 1.008 1.74 3.66 1.824 0 0 .444-.54.804-.996-1.524-.456-2.1-1.416-2.1-1.416l.336.204.048.036.047.027.014.006.047.027c.3.168.6.3.876.408.492.192 1.08.384 1.764.516.9.168 1.956.228 3.108.012.564-.096 1.14-.264 1.74-.516.42-.156.888-.384 1.38-.708 0 0-.6.984-2.172 1.428.36.456.792.972.792.972zm-5.58-5.604c-.684 0-1.224.6-1.224 1.332 0 .732.552 1.332 1.224 1.332.684 0 1.224-.6 1.224-1.332.012-.732-.54-1.332-1.224-1.332zm4.38 0c-.684 0-1.224.6-1.224 1.332 0 .732.552 1.332 1.224 1.332.684 0 1.224-.6 1.224-1.332 0-.732-.54-1.332-1.224-1.332z" />
   </svg>
 </button>
             </div>
@@ -156,11 +178,34 @@
   .logo{
     width: 1em;
   }
+  .home_nav li a{
+    
+    font-size: 0.3em;
+    
+  }
 
+  .relative{
+flex-direction: column;
+    font-size: 0.3em;
+    
+  }
+
+  .line{
+display: none;
+    
+  }
 
 
   }
 
+  @media screen and (min-width: 1024px) {
+
+  .home_nav li a{
+    
+display: flex;
+flex-direction: row;
+}
+  }
 
 
 </style>
